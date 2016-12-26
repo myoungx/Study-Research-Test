59cfe4156a623949880ccf0c2b9c5680d8455f6e
mengyang <mengyang05@meituan.com>
2016-12-26 23:41:51 +0800

    测试内存的分支

--- app/src/main/java/youngmeng/org/studyandresearch/drawablematch/DrawableTestActivity.java
+++ app/src/main/java/youngmeng/org/studyandresearch/drawablematch/DrawableTestActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import youngmeng.org.studyandresearch.R;
@@ -13,14 +12,6 @@ public class DrawableTestActivity extends AppCompatActivity {
 
     private TextView xdpiTextView;
     private TextView ydpiTextView;
-
-    private TextView width_1x;
-    private TextView height_1x;
-    private TextView width_3x;
-    private TextView height_3x;
-
-    private ImageView smallImage;
-    private ImageView bigImage;
 
     private Button showWidthHeight;
 
@@ -31,17 +22,9 @@ public class DrawableTestActivity extends AppCompatActivity {
 
         showWidthHeight = (Button) findViewById(R.id.show_width_height);
 
-        smallImage = (ImageView) findViewById(R.id.image_small);
-        bigImage = (ImageView) findViewById(R.id.image_big);
 
         xdpiTextView = (TextView) findViewById(R.id.xdpi);
         ydpiTextView = (TextView) findViewById(R.id.ydpi);
-
-        width_1x = (TextView) findViewById(R.id.width_1x);
-        height_1x = (TextView) findViewById(R.id.height_1x);
-
-        width_3x = (TextView) findViewById(R.id.width_3x);
-        height_3x = (TextView) findViewById(R.id.height_3x);
 
         float xdpi = getResources().getDisplayMetrics().xdpi;
         float ydpi = getResources().getDisplayMetrics().ydpi;
@@ -53,11 +36,6 @@ public class DrawableTestActivity extends AppCompatActivity {
         showWidthHeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                width_1x.setText("width:" + smallImage.getWidth());
-                height_1x.setText("---height:" + smallImage.getHeight());
-
-                width_3x.setText("width:" + bigImage.getWidth());
-                height_3x.setText("---height:" + bigImage.getHeight());
             }
         });
     }
